3d128915c28eb954f755ecc286e6fc5e63094f3e
Yagiz Hitit <yagizhitit@gmail.com>
2024-12-25 02:37:06 +0000

    Initial Commit

---
+++ AkakceApplication/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AkakceApplication
+//
+//  Created by Yağız Hitit on 25.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ AkakceApplicationTests/AkakceApplicationTests.swift
@@ -0,0 +1,17 @@
+//
+//  AkakceApplicationTests.swift
+//  AkakceApplicationTests
+//
+//  Created by Yağız Hitit on 25.12.2024.
+//
+
+import Testing
+@testable import AkakceApplication
+
+struct AkakceApplicationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
